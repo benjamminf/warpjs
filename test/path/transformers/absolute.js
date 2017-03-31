dfbe6be27a9899916fb00f3aa886971052dda51f
Benjamin Fleming <benjfleming21@gmail.com>
2017-03-31 12:23:28 +1100

    Added tests to complete coverage of absolute transformer

--- test/path/transformers/absolute.js
+++ test/path/transformers/absolute.js
@@ -27,4 +27,43 @@ describe('absolute()', function()
 			{ type: 'l', relative: true, x: 4, y: 5 },
 		], absolute()))
 	})
+
+	it('should correctly handle closing paths followed by a move segment', function()
+	{
+		assert.deepEqual([
+			{ type: 'm', relative: false, x: 0, y: 1 },
+			{ type: 'l', relative: false, x: 2, y: 4 },
+			{ type: 'z', relative: false },
+			{ type: 'm', relative: false, x: 4, y: 6 },
+		], transformer([
+			{ type: 'm', relative: false, x: 0, y: 1 },
+			{ type: 'l', relative: true, x: 2, y: 3 },
+			{ type: 'z', relative: true },
+			{ type: 'm', relative: true, x: 4, y: 5 },
+		], absolute()))
+	})
+
+	it('should correctly handle closing paths followed by a drawing segment', function()
+	{
+		assert.deepEqual([
+			{ type: 'm', relative: false, x: 0, y: 1 },
+			{ type: 'l', relative: false, x: 2, y: 4 },
+			{ type: 'z', relative: false },
+			{ type: 'l', relative: false, x: 4, y: 6 },
+		], transformer([
+			{ type: 'm', relative: false, x: 0, y: 1 },
+			{ type: 'l', relative: true, x: 2, y: 3 },
+			{ type: 'z', relative: true },
+			{ type: 'l', relative: true, x: 4, y: 5 },
+		], absolute()))
+	})
+
+	it('should correctly handle paths starting with a drawing segment', function()
+	{
+		assert.deepEqual([
+			{ type: 'l', relative: false, x: 0, y: 1 },
+		], transformer([
+			{ type: 'l', relative: true, x: 0, y: 1 },
+		], absolute()))
+	})
 })
