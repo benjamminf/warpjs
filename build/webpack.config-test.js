db97a02756d14086ca5ce07690d4d4297782bd55
Benjamin Fleming <benjfleming21@gmail.com>
2017-03-31 12:10:26 +1100

    Added test coverage

--- build/webpack.config-test.js
+++ build/webpack.config-test.js
@@ -2,4 +2,17 @@ const config = require('./webpack.config')
 
 config.target = 'node'
 
+for(let rule of config.module.rules)
+{
+	if(rule.use && rule.use.loader === 'babel-loader')
+	{
+		rule.use.options = rule.use.options || {}
+		rule.use.options.env = rule.use.options.env || {}
+		
+		rule.use.options.env.test = {
+			plugins: ['istanbul'],
+		}
+	}
+}
+
 module.exports = config
