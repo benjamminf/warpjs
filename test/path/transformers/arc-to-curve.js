77d73d67dabac0c5c8fca81253405cd90888b9f5
Benjamin Fleming <benjfleming21@gmail.com>
2017-04-01 22:45:29 +1100

    Added test for relative arcs

--- test/path/transformers/arc-to-curve.js
+++ test/path/transformers/arc-to-curve.js
@@ -5,7 +5,7 @@ import arcToCurve from '../../../src/path/transformers/arc-to-curve'
 
 describe('arcToCurve()', function()
 {
-	it('should not transform if all segments are already absolute', function()
+	it('should convert absolute arc to one absolute cubic bezier curve', function()
 	{
 		approxDeepEqual([
 			{ type: 'm', relative: false, x: 100, y: 100 },
@@ -13,6 +13,17 @@ describe('arcToCurve()', function()
 		], transformer([
 			{ type: 'm', relative: false, x: 100, y: 100 },
 			{ type: 'a', relative: false, rx: 100, ry: 100, xRotation: 0, largeArc: false, sweep: true, x: 200, y: 200 },
+		], arcToCurve()), 0.01)
+	})
+
+	it('should convert relative arc to one relative cubic bezier curve', function()
+	{
+		approxDeepEqual([
+			{ type: 'm', relative: false, x: 100, y: 100 },
+			{ type: 'c', relative: true, x1: 55.23, y1: 0, x2: 100, y2: 44.77, x: 100, y: 100 },
+		], transformer([
+			{ type: 'm', relative: false, x: 100, y: 100 },
+			{ type: 'a', relative: true, rx: 100, ry: 100, xRotation: 0, largeArc: false, sweep: true, x: 100, y: 100 },
 		], arcToCurve()), 0.01)
 	})
 })
