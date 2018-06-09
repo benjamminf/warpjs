07192af2621b693811f0030877ce7e2da591d723
Benjamin Fleming <benjfleming21@gmail.com>
2018-06-09 11:01:11 +1000

    Wave transformer progress

--- src/warp/transformers/wave.js
+++ src/warp/transformers/wave.js
@@ -1,20 +1,37 @@
-export default function waveTransformerFactory(wavelength, amplitude, angle=0, offset=0, type='sine')
-{
-	return function waveTransformer([ x, y, ...points ])
-	{
-		return [
-			x,
-			y,
-			...points,
-		]
-	}
+const modes = {
+	sine: (t, l, a, o) => a * Math.sin((2 * Math.PI * t + o) / l),
+	square: (t, l, a, o, d=0.5) => (t - o) % l < d * l ? a : -a,
+	//triangle: (t, l, a, o) => ,
+	//sawtooth: (t, l, a, o) => ,
 }
 
-const warp = new Warp
-const { wave } = Warp
+/**
+ *
+ * @param wavelength
+ * @param amplitude
+ * @param angle
+ * @param offset
+ * @param mode
+ * @returns {function(*[]): *[]}
+ */
+export default function waveFactory(wavelength, amplitude, angle=90, offset=0, mode='sine')
+{
+	const modeFunction = typeof mode === 'function' ? mode : modes[mode]
 
-const init = ([ x, y ]) => [ x, y, x, y ]
-const reset = ([ x, y, ox, oy ]) => [ ox, oy, ox, oy ]
+	return function wave([ x, y, ...points ])
+	{
+		// Rotate by angle
+		const x1 = x * Math.cos(-angle) - y * Math.sin(-angle)
+		const y1 = x * Math.sin(-angle) + y * Math.cos(-angle)
 
-warp.transform(init)
-warp.transform([ reset, wave(50, 30) ])
+		// Apply y axis wave
+		const x2 = x1
+		const y2 = y1 + modeFunction(x1, wavelength, amplitude, offset)
+
+		// Rotate back to normal
+		const x3 = x2 * Math.cos(angle) - y2 * Math.sin(angle)
+		const y3 = x2 * Math.sin(angle) + y2 * Math.cos(angle)
+
+		return [ x3, y3, ...points ]
+	}
+}
