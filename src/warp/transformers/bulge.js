c36a849623b6b661189e77ee849ac18f1bdfe0d7
Benjamin Fleming <benjfleming21@gmail.com>
2018-06-11 18:03:18 +1000

    Bulge effect basic setup

--- src/warp/transformers/bulge.js
+++ src/warp/transformers/bulge.js
@@ -2,9 +2,18 @@ export default function bulgeFactory(radius, weight, cx=0, cy=0)
 {
 	return function bulge([ x, y, ...points ])
 	{
+		const ox = x - cx
+		const oy = y - cy
+		const rx = ox / radius
+		const ry = oy / radius
+		const r = Math.sqrt(rx**2 + ry**2)
+		const theta = Math.atan2(ry, rx)
+		const rn = 2 * r**2.5
+		const rw = r * (1 - weight) - rn * weight
+
 		return [
-			x,
-			y,
+			rw * Math.cos(theta) * radius + cx,
+			rw * Math.sin(theta) * radius + cy,
 			...points,
 		]
 	}
